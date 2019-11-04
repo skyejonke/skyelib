63643f3a73da7934eb89a5c0e0524e0356a96df5
Skye J <skye@skyenet.online>
2019-11-03 21:43:44 -0500

    Added reset method to toolkit

--- skyelib.cpp
+++ skyelib.cpp
@@ -17,3 +17,12 @@ int toolkit::getRand(int start, int end){
 	return distr(eng);
 }
 
+void toolkit::replace(std::string& str,
+               const std::string& oldStr,
+               const std::string& newStr){
+       std::string::size_type pos = 0u;
+       while((pos = str.find(oldStr, pos)) != std::string::npos){
+		str.replace(pos, oldStr.length(), newStr);
+		pos += newStr.length();
+	}
+}
--- skyelib.hpp
+++ skyelib.hpp
@@ -8,5 +8,9 @@ class toolkit{
 	public:
 		std::vector<std::string> splitString(std::string inpt);
 		int getRand(int start, int end);
+		void replace(std::string& str,
+			const std::string& oldStr,
+			const std::string& newStr);
 };
+
 #endif
