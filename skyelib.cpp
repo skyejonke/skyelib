8f5873e4a2aed0716278ef12889d75d85e280961
Skye J <skye@skyenet.online>
2019-11-22 09:55:41 -0500

    Added some stuff

--- skyelib.cpp
+++ skyelib.cpp
@@ -34,3 +34,12 @@ void toolkit::replace(std::string& str,
 		pos += newStr.length();
 	}
 }
+void skyelib::replace(std::string& str,
+               const std::string& oldStr,
+               const std::string& newStr){
+       std::string::size_type pos = 0u;
+       while((pos = str.find(oldStr, pos)) != std::string::npos){
+		str.replace(pos, oldStr.length(), newStr);
+		pos += newStr.length();
+	}
+}
