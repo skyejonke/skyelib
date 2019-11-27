8bdaa5db709c9499dc567c1bef1ad5630c9048ef
Skye J <skye@skyenet.online>
2019-11-27 11:47:01 -0500

    Added travis and test support

---
+++ test/test.cpp
@@ -0,0 +1,14 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include "../skyelib.hpp"
+#include <string>
+
+using namespace std;
+TEST_CASE("basic skyelib tests", "[skyelib]"){
+	toolkit* tools = new toolkit();
+	string testString = "I like <food>.";
+	tools->replace(testString,"<food>","pie");
+	REQUIRE(testString == "I like pie.");
+
+}
+
